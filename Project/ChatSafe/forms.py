d1a7f882f9e1aeb3d2a758dd43588c8e6ee63fc5
Akram Hammam <63311262+akramhammam5@users.noreply.github.com>
2024-01-10 01:46:59 +0200

    Delete Project directory

--- Project/ChatSafe/forms.py
+++
@@ -1,6 +0,0 @@
-# audiowatermark/forms.py
-from django import forms
-
-class AudioWatermarkForm(forms.Form):
-    text_input = forms.CharField(label='Text Input', max_length=100)
-    audio_file = forms.FileField(label='MP3 File')
--- Project/ChatSafe/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Mp3File(models.Model):
-    original_file = models.FileField(upload_to='mp3_uploads/')
-    modified_file = models.FileField(upload_to='modified_mp3/', blank=True)
-    hidden_text = models.TextField()
-
