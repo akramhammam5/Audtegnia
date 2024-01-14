ca9c92f3e03a3a61ea5b9cb2d28e065f697622a6
omarhishamxx <72707592+omarhishamxx@users.noreply.github.com>
2024-01-14 21:22:20 +0200

    added decryption

--- ChatSafe/Audstegnia/urls.py
+++ ChatSafe/Audstegnia/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 from .views import * 
 urlpatterns = [
-    path('', form_template_view , name='form_template_view'),
+    path('', hide , name='form_template_view'),
+    path('extract/', extract , name='extract'),
 ]
 
 
--- ChatSafe/Audstegnia/views.py
+++ ChatSafe/Audstegnia/views.py
@@ -1,18 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .utils import embed_audio_message
+from .utils import *
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from base64 import b64encode, b64decode
 
-def form_template_view(request):
+
+
+encryption_key = "mysecretkey"
+
+
+def hide(request):
     if request.method == 'POST':
-        audiofile = request.FILES['file']
-        secretmsg = request.POST['text']
+        audiofile = request.FILES.get('file')
+        secretmsg = request.POST.get('text')
         outputfile = '/home/elliot/Documents/Grad/Implementation/Virtual/ChatSafe/audio/output.wav'
-
-        success = embed_audio_message(audiofile, secretmsg, outputfile)
-
-        if success:
-                return render(request, 'success.html', {'outputfile': outputfile})
-        else:
-                return render(request, 'error.html')
+        if audiofile and secretmsg and outputfile:
+            try:
+                em_audio(audiofile, secretmsg , outputfile)
+                return HttpResponse("Message hidden successfully!")
+            except Exception as e:
+                return HttpResponse(f"Error hiding message: {str(e)}")
     return render(request, 'index.html')
+        
+        
+def extract(request):
+    if request.method == 'POST':
+        audiofile = request.FILES.get('file')
+        if audiofile:
+            try:
+                extracted_message = ex_msg(audiofile)
+                return render(request, 'extracted_message.html', {'secret_message': extracted_message})
+            except Exception as e:
+                return render(request, 'error.html', {'error_message': f"Error extracting message: {str(e)}"})
+    return render(request, 'Decode.html')
+
+
 
